ffee8462321e586c8b8c19465b3a31ab9baead0f
BlaiseGratton <blaisegratton@gmail.com>
2016-05-21 14:22:37 +0300

    change layout to use table; appending new entries to table

--- js/detail.js
+++ js/detail.js
@@ -3,7 +3,14 @@
 
     var dbService = DbService('http://kenchreai.org/kaa/');
     var urlService = UrlService(window)('http://kenchreai.org/kaa/');
-    var resultsList = $('#results-list');
+    var attributeList = $('#attribute-list tbody');
+    var resourceTitle = urlService.getHash();
+    var newData = [];
+
+    $('#add-btn').click(addEntry);
+
+    $('#resource-title').text('Details for ' + resourceTitle);
+    $('title').text('Details - ' + resourceTitle);
 
     dbService.getDetail(urlService.getResourceFromHash(), function(response) {
       var results = response.results.bindings;
@@ -12,9 +19,23 @@
         for (var key in result) {
           descriptors.push(result[key].value);
         }
-        var elem = $('<li>' + descriptors.join(' ') + '</li>');
-        resultsList.append(elem);
+        var elem = $('<tr><td>' + descriptors[0] + '</td><td>' + descriptors[1] + '</td></tr>');
+        attributeList.append(elem);
       });
     });
+
+    function addEntry(e) {
+      e.preventDefault();
+      var key = $('input[name="key"]').val();
+      var value = $('input[name="value"]').val();
+      if (key && value) {
+        var elem = $('<tr><td>' + key + '</td><td>' + value + '</td></tr>');
+        attributeList.append(elem);
+        newData.append({ key: key, value: value });
+        $('input[name="key"]').val('');
+        $('input[name="value"]').val('');
+      }
+    }
+
   });
 })(jQuery, DbService, UrlService);
