b82e0d7ca65f3ff568160e363782e6d022acc419
BlaiseGratton <blaisegratton@gmail.com>
2016-05-21 11:02:19 +0300

    add basic detail view loading of inventory items

---
+++ js/detail.js
@@ -0,0 +1,20 @@
+;(function() {
+  $(document).ready(function() {
+
+    var dbService = DbService('http://kenchreai.org/kaa/');
+    var detailUrl = window.location.hash.slice(1);
+    var resultsList = $('#results-list');
+
+    dbService.getDetail(detailUrl, function(response) {
+      var results = response.results.bindings;
+      results.forEach(function(result) {
+        var descriptors = [];
+        for (var key in result) {
+          descriptors.push(result[key].value);
+        }
+        var elem = $('<li>' + descriptors.join(' ') + '</li>');
+        resultsList.append(elem);
+      });
+    });
+  });
+})(jQuery, DbService);
