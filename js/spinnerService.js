2fab5328e190949e6e6dff3094854b8cba8b493f
BlaiseGratton <blaisegratton@gmail.com>
2016-05-21 17:53:23 +0300

    clean up config object for spinner

--- js/spinnerService.js
+++ js/spinnerService.js
@@ -5,26 +5,26 @@
       var target = $('#spinner-target');
       
       var options = {
-        lines: 13 // The number of lines to draw
-      , length: 28 // The length of each line
-      , width: 14 // The line thickness
-      , radius: 42 // The radius of the inner circle
-      , scale: 1 // Scales overall size of the spinner
-      , corners: 1 // Corner roundness (0..1)
-      , color: '#000' // #rgb or #rrggbb or array of colors
-      , opacity: 0.25 // Opacity of the lines
-      , rotate: 0 // The rotation offset
-      , direction: 1 // 1: clockwise, -1: counterclockwise
-      , speed: 1 // Rounds per second
-      , trail: 60 // Afterglow percentage
-      , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-      , zIndex: 2e9 // The z-index (defaults to 2000000000)
-      , className: 'spinner' // The CSS class to assign to the spinner
-      , top: '100%' // Top position relative to parent
-      , right: '-50%' // Left position relative to parent
-      , shadow: false // Whether to render a shadow
-      , hwaccel: false // Whether to use hardware acceleration
-      , position: 'relative'
+        lines: 13,
+        length: 28,
+        width: 14,
+        radius: 42,
+        scale: 1,
+        corners: 1,
+        color: '#000',
+        opacity: 0.25,
+        rotate: 0,
+        direction: 1,
+        speed: 1,
+        trail: 60,
+        fps: 20,
+        zIndex: 2e9,
+        className: 'spinner',
+        top: '100%',
+        right: '-50%',
+        shadow: false,
+        hwaccel: false,
+        position: 'relative'
       };
 
       var spinner = new Spinner(options);
