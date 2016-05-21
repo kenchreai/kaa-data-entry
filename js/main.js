9134c2d2e043e53dbe9a2d67926e16f4cc51389a
BlaiseGratton <blaisegratton@gmail.com>
2016-05-21 11:08:25 +0300

    main page with styling

--- js/main.js
+++ js/main.js
@@ -1,5 +1,46 @@
 ;(function() {
   $(document).ready(function() {
-    console.log('hi');
+
+    var dbService = DbService('http://kenchreai.org/kaa/');
+    var resultsList = $('#results-list');
+
+    var cachedObj = location.hash ? location.hash.slice(1) : null;
+
+    if (cachedObj) {
+      var response = JSON.parse(sessionStorage.getItem(cachedObj));
+      if (response) {
+        printResults(response);
+      }
+    }
+
+    $('#search').on('click', function(e) {
+      var entity = $('#entity-field').val()
+      $('#no-results').remove();
+      $('#results-list').children().remove();
+      dbService.query({ entity: entity }, function(response) {
+        var results = response.results.bindings;
+        if (results.length === 0) {
+          $('#results-list').append($('<p id="no-results">No results found for query "' + params.entity + '".</p>'));
+        } else {
+          printResults(results);
+        }
+      });
+      e.preventDefault();
+    });
+
+    function printResults(results) {
+      results.forEach(function(result) {
+        var descriptors = [];
+        for (var key in result) {
+          descriptors.push(result[key].value);
+        }
+        var elem = $('<li><a href="/detail/#' + descriptors.join(' ') + '">' + descriptors.join(' ') + '</a></li>');
+        resultsList.append(elem);
+      });
+      var encodedResults = JSON.stringify(results);
+      sessionStorage.setItem('cats', encodedResults);
+      location.hash = 'cats';
+    }
+
   });
-})(jQuery)
+})(jQuery, DbService)
