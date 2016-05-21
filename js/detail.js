821d578ec8a76e4528c1bc5a9e314d29f6ed201f
BlaiseGratton <blaisegratton@gmail.com>
2016-05-21 15:33:24 +0300

    change text input to select element

--- js/detail.js
+++ js/detail.js
@@ -12,24 +12,34 @@
     $('#resource-title').text('Details for ' + resourceTitle);
     $('title').text('Details - ' + resourceTitle);
 
-    dbService.getDetail(urlService.getResourceFromHash(), function(response) {
-      var results = response.results.bindings;
-      results.forEach(function(result) {
-        var descriptors = [];
-        try {
-          descriptors.push(result.label.value);
-        } catch(e) {
-          descriptors.push(result.p.value);
-        }
-        descriptors.push(result.o.value);
-        var elem = $('<tr><td>' + descriptors[0] + '</td><td>' + descriptors[1] + '</td></tr>');
-        attributeList.append(elem);
-      });
+    var dropDown = $('#key-value-pairs select');
+    var fakeData = ['Is a', 'Label', 'Logical part of', 'Photo'];
+
+    fakeData.forEach(function(descriptor) {
+      dropDown.append($('<option value="' + descriptor +'">' + descriptor + '</option>'));
     });
+
+    function loadDetail() {
+      dbService.getDetail(urlService.getResourceFromHash(), function(response) {
+        attributeList.children().remove();
+        var results = response.results.bindings;
+        results.forEach(function(result) {
+          var descriptors = [];
+          try {
+            descriptors.push(result.label.value);
+          } catch(e) {
+            descriptors.push(result.p.value);
+          }
+          descriptors.push(result.o.value);
+          var elem = $('<tr><td>' + descriptors[0] + '</td><td>' + descriptors[1] + '</td></tr>');
+          attributeList.append(elem);
+        });
+      });
+    }
 
     function addEntry(e) {
       e.preventDefault();
-      var key = $('input[name="key"]').val();
+      var key = $('select[name="key"]').val();
       var value = $('input[name="value"]').val();
       if (key && value) {
         var elem = $('<tr><td>' + key + '</td><td>' + value + '</td></tr>');
