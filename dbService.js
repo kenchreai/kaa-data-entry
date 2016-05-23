d85d42103b0488228231185b37dea4161f5ed728
BlaiseGratton <blaisegratton@gmail.com>
2016-05-23 14:19:44 +0300

    transition to server side communication with stardog

---
+++ dbService.js
@@ -0,0 +1,65 @@
+;(function() {
+  var Stardog = require('stardog');
+  var DbService = (function() {
+    return function(baseUrl, username, password) { 
+      
+      var conn = new Stardog.Connection();
+      conn.setEndpoint("http://kenchreai.org:5820/");
+      conn.setReasoning(false);
+      conn.setCredentials(username, password);
+      
+      var dbConfig = {
+        database: 'kenchreai'
+      };
+
+      function query(params, cb) {
+        var options = Object.assign({}, dbConfig);
+        //options.query = 'describe <http://kenchreai.org/kaa/test/test01>';
+        //options.query = 'select ?p ?o where { <http://kenchreai.org/kaa/harbor/ke1221> ?p ?o }';
+        options.query = 'select ?s ?p where { ?s a kaaont:inventory-number . ?s kaaont:is-logical-part-of <' + baseUrl + 'kth/inventoried-objects> } order by ?s';
+        //options.query = 'select ?s ?p where { ?s a kaaont:inventory-number . ?s kaaont:is-logical-part-of+/kaa:' + params.entity + ' } order by ?s';
+        conn.query(options, function(response) {
+          cb(response);
+        });
+      }
+
+      function getDetail(detailUrl, cb) {
+        var options = Object.assign({}, dbConfig);
+        options.query = 'select ?p ?o ?label where { <' +
+                        detailUrl + '> ?p ?o . optional {' +
+                        'graph <urn:kenchreai:schema> { ?p rdfs:label ?label } } }';
+        conn.query(options, function(response) {
+          cb(response);
+        });
+      }
+
+      function getDescriptors(cb) {
+        var options = Object.assign({}, dbConfig);
+        options.query = 'SELECT ?s ?p ?o ?type ?domain ?range ?label WHERE {' +
+                          'graph <urn:kenchreai:schema> {' +
+                            '{ ?s rdf:type owl:ObjectProperty . ?s rdf:type ?type . }' +
+                            'UNION { ?s rdf:type owl:DatatypeProperty . ?s rdf:type ?type . }' +
+                            'OPTIONAL { ?s rdfs:label ?label . }' +
+                            'OPTIONAL { ?s rdfs:domain ?domain . }' +
+                            'OPTIONAL { ?s rdfs:range ?range . }' +
+                            'FILTER ( ?type = owl:ObjectProperty || ?type = owl:DatatypeProperty )' +
+                          '}' +
+                        '}';
+        conn.query(options, function(response) {
+          cb(response);
+        });
+      }
+
+      return {
+        query: query,
+        getDetail: getDetail,
+        getDescriptors: getDescriptors
+      };
+    };
+  })();
+
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
+    module.exports = DbService;
+  else
+    window.DbService = DbService;
+})();
---
+++ server.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var http = require('http').Server(app);
+var path = require('path');
+var env = require('dotenv').config();
+var DbService = require('./dbService.js');
+
+var username = process.env.KENCHREAI_USER;
+var password = process.env.KENCHREAI_PASSWORD;
+var dbService = DbService('http://kenchreai.org/kaa/', username, password);
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
+  next();
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+/*****************      routes     ********************/
+
+app.post('/api/query', function(req, res) {
+  var params = req.body;
+  dbService.query(params, function(response) {
+    res.send(response);
+  });
+});
+
+app.post('/api/getdetail', function(req, res) {
+  var resource = req.body.resource;
+  console.log(resource);
+  dbService.getDetail(resource, function(response) {
+    res.send(response);
+  });
+});
+
+app.get('/api/descriptors', function(req, res) {
+  dbService.getDescriptors(function(response) {
+    res.send(response);
+  });
+});
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+/******************************************************/
+
+http.listen(port, function() {
+  console.log('listening on *:' + port);
+});
