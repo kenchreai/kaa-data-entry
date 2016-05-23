2e46707621947268d42ffe16b285002f6f2b2207
BlaiseGratton <blaisegratton@gmail.com>
2016-05-23 15:19:24 +0300

    still trying

--- server.js
+++ server.js
@@ -49,6 +49,14 @@ app.get('/api/descriptors', function(req, res) {
   });
 });
 
+app.get('node_modules/jquery/dist/jquery.js', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/node_modules/jquery/dist/jquery.js'));
+});
+
+app.get('node_modules/spin/dist/spin.min.js', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/node_modules/spin/dist/spin.min.js'));
+});
+
 app.get(function(req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
