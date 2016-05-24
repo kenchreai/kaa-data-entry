2c629a3b79914eeb61ca006b7c2cbbe2c2977945
BlaiseGratton <blaisegratton@gmail.com>
2016-05-24 12:11:16 +0300

    passing tests for string, integer, and float validation

---
+++ public/js/validator.js
@@ -0,0 +1,46 @@
+;(function() {
+  var Validator = (function(){
+    return function() {
+
+      function str(input) {
+        return (!!input);
+      }
+
+      function integer(input) {
+        if (input.indexOf('.') !== -1) return false;
+        return input >= 0;
+      }
+
+      function float(input) {
+        return parseFloat(input) >= 0;
+      }
+
+      function validate(type, input) {
+        if (!input) return false;
+
+        switch(type) {
+          case 'string':
+            return str(input);
+            break;
+          case 'integer':
+            return integer(input);
+            break;
+          case 'float':
+            return float(input);
+            break;
+          default:
+            return false;
+        }
+      }
+
+      return {
+        validate: validate
+      };
+    };
+  })();
+
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
+    module.exports = Validator;
+  else
+    window.Validator = Validator;
+})();
---
+++ test/validatorTests.js
@@ -0,0 +1,94 @@
+'use strict';
+
+var assert = require('chai').assert;
+var Validator = require('./../public/js/validator.js');
+
+describe('Instantiating the module', function() {
+  
+  it('should not be null', function() {
+    var val = Validator();
+    assert.isOk(val);
+  });
+});
+
+describe('Passing in strings to be evaluates', function() {
+
+  var val = Validator();
+
+  describe('Validating strings', function() {
+
+    it('should basically accept most things', function() {
+      assert.isTrue(val.validate('string', 'Koutsongilas Ridge'));
+      assert.isTrue(val.validate('string', 'Chloe loves Gavin'));
+      assert.isTrue(val.validate('string', 'We aren\'t really concerned with limiting the length of strings because we assume people won\'t be idiots'));
+    });
+
+    it('should not accept empty inputs', function() {
+      assert.isFalse(val.validate('string'));
+      assert.isFalse(val.validate('string', ''));
+      assert.isFalse(val.validate('string', false));
+      assert.isFalse(val.validate('string', null));
+    });
+
+  });
+
+  describe('Validating integers', function() {
+
+    it('should validate only positive integers', function() {
+      assert.isTrue(val.validate('integer', '1'));
+      assert.isTrue(val.validate('integer', '0'));
+      assert.isTrue(val.validate('integer', '1234567890'));
+      assert.isTrue(val.validate('integer', '10e2345'));
+      assert.isFalse(val.validate('integer', 'arstdhneio'));
+    });
+
+    it('should reject negative numbers', function() {
+      assert.isFalse(val.validate('integer', '-1'));
+      assert.isFalse(val.validate('integer', '-1,000'));
+      assert.isFalse(val.validate('integer', ''));
+    });
+
+    it('should reject floats', function() {
+      assert.isFalse(val.validate('integer', '.1'));
+      assert.isFalse(val.validate('integer', '-4.5'));
+      assert.isFalse(val.validate('integer', '400.5'));
+    });
+    
+  });
+
+  describe('Validating floats', function() {
+
+    it('should accept valid positive floats', function() {
+      assert.isTrue(val.validate('float', '.1'));
+      assert.isTrue(val.validate('float', '10.2'));
+      assert.isTrue(val.validate('float', '0.0'));
+      assert.isTrue(val.validate('float', '0.8'));
+      assert.isTrue(val.validate('float', '1234.5678'));
+      assert.isFalse(val.validate('float', 'oienwtf.5678'));
+    });
+
+    it('should also accept standard integers', function() {
+      assert.isTrue(val.validate('float', '10'));
+      assert.isTrue(val.validate('float', '0'));
+      assert.isTrue(val.validate('float', '10e38'));
+    });
+
+    it('should reject negative numbers', function() {
+      assert.isFalse(val.validate('float', '-10.3'));
+      assert.isTrue(val.validate('float', '-0.0'));
+    });
+
+  });
+
+  describe('Validating booleans', function() {
+
+    it('should');
+
+  });
+
+  describe('Validating URIs', function() {
+
+    it('should');
+
+  });
+});
